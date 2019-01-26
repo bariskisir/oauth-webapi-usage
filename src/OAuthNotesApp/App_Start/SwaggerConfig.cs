da6138107a342d6840e61bda5f95b5a6db863232
Barış Kısır <bariskisir@gmail.com>
2019-01-26 20:19:14 +0300

    swagger ui added

---
+++ src/OAuthNotesApp/App_Start/SwaggerConfig.cs
@@ -0,0 +1,19 @@
+﻿using OAuthNotesApp.App_Start;
+using Swashbuckle.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "RegisterSwaggerConfig")]
+namespace OAuthNotesApp.App_Start
+{
+    public class SwaggerConfig
+    {
+        public static void RegisterSwaggerConfig()
+        {
+            var assembly = typeof(SwaggerConfig).Assembly;
+            GlobalConfiguration.Configuration.EnableSwagger(x => x.SingleApiVersion("v1", "NotesAPI")).EnableSwaggerUi();
+        }
+    }
+}
